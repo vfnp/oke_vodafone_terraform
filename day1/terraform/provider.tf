6d3fd16cfe737bc8acbd3302e3f65f1965d88e4f
vfnp <152149850+vfnp@users.noreply.github.com>
2023-11-27 12:37:13 +0530

    Create provider.tf

---
+++ day1/terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "oci" {
+    region = "ap-mumbai-1"
+    tenancy_ocid    = "ocid1.tenancy.oc1..aaaaaaaabyckwkntv5pb2fe62pruenyquabphyppew5oj4c27rtex5uur5qq"
+    user_ocid = "ocid1.user.oc1..aaaaaaaawaalcawnvlm3pmldbg5rq6f32hxifqmgg3k4fxlprnwb5hw5726q"
+}
